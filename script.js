dd664866d6bee5f1763312d3e09463dbc0a32652
Dirad-01 <tidirad@dirad.com.br>
2026-04-06 10:58:10 -0300

    Update script.js
    atualizaoa do bug

--- script.js
+++ script.js
@@ -363,29 +363,22 @@ EAP ( ) Sangramento intenso ( ) Pneumotórax ( ) Outros ( ) Qual: _____
 			 "","","","","","","simnao"
 			 ]
   },
-  
-  : {
-    titulo: "Transferencia de Paciente interno em outra instituição",
+   Novomodelo2: {
+    titulo: "Novo modelo 2",
     texto: `
-CONDIÇÕES PARA A TRANSFERÊNCIA
-Paciente em uso de algum dispositivo? _____
-Qual: ____ Calibre: ____ Membro: ____ Data do acesso: ____
-Paciente respirando em ambiente? _____ 
-Dispositivo: _____ 
-Paciente realizou algum tipo de preparo? _____ 
-Medicamento? _____
-Droga: ____ Via de administração: ____ Vol/h: ____
-SSVV: PA: _x_ mmHg FC: __ bpm FR: __ irpm T: _°C SpO2: __ % Dor: ___
 
-INDICAÇÃO DA TRANSFERÊNCIA
-Condições clínicas relacionadas ao paciente? Sim ( ) Não ( )
-Reação anafilática ( ) Extravasamento de contraste ( ) Risco de injúria renal ( ) Queda ( )
-EAP ( ) Sangramento intenso ( ) Pneumotórax ( ) Outros ( ) Qual: ____
+Matérias descartáveis utilizados:
+
+Esparadrapo: _____, quantidade: _____
+Seringa: _____, quantidade: _____
+Agulha:  _____, quantidade: _____
+Gase:    _____, quantidade: _____
+Luva:    _____, quantidade: _____
+Seringa salinizada:  _____, quantidade: _____
+Outros:  _____, quantidade: _____
 `,
-    campos: ["simnao","","","membro","data","simnao","Dispositivo","simnao","","",
-			 "","","","","","",""]
-	
-	
+    campos: ["Esparadrapo","number","Seringa","number",
+			"Agulha","number","","number","Luva","number","","number",""]
   },
    Novomodelo3: {
     titulo: "Novo modelo 3",
